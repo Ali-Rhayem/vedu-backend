dc5a5894de17c018493c3d7a607f632155475680
Alirh1 <alirhayem.1212@gmail.com>
2024-08-19 17:59:15 +0300

    added course instuctor table

---
+++ vedu-backend/app/Http/Controllers/CourseInstructorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CourseInstructor;
+use App\Http\Requests\StoreCourseInstructorRequest;
+use App\Http\Requests\UpdateCourseInstructorRequest;
+
+class CourseInstructorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCourseInstructorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CourseInstructor $courseInstructor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CourseInstructor $courseInstructor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCourseInstructorRequest $request, CourseInstructor $courseInstructor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CourseInstructor $courseInstructor)
+    {
+        //
+    }
+}
