04632a0f8f25784fe58a29828500ac802d452f85
Alirh1 <alirhayem.1212@gmail.com>
2024-08-19 18:46:49 +0300

    added livecoding session migration,model and controller

---
+++ vedu-backend/app/Http/Controllers/LiveCodingSessionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LiveCodingSession;
+use App\Http\Requests\StoreLiveCodingSessionRequest;
+use App\Http\Requests\UpdateLiveCodingSessionRequest;
+
+class LiveCodingSessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLiveCodingSessionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LiveCodingSession $liveCodingSession)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LiveCodingSession $liveCodingSession)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLiveCodingSessionRequest $request, LiveCodingSession $liveCodingSession)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LiveCodingSession $liveCodingSession)
+    {
+        //
+    }
+}
---
+++ vedu-backend/database/migrations/2024_08_19_154625_create_live_coding_sessions_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('live_coding_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('live_coding_sessions');
+    }
+};
