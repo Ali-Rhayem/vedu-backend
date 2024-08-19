6c48f2e222a6536f1ee1477ddbcfc06267998e18
Alirh1 <alirhayem.1212@gmail.com>
2024-08-19 21:57:19 +0300

    added Jwt to user model

--- vedu-backend/app/Models/User.php
+++ vedu-backend/app/Models/User.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -75,5 +75,25 @@ class User extends Authenticatable
     public function submissions()
     {
         return $this->hasMany(Submission::class, 'student_id');
+    }
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
