58d779737197d7d6e9b3b38f70074bd6252a6bfd
Alirh1 <alirhayem.1212@gmail.com>
2024-08-19 18:20:06 +0300

    updated create chats table migration

--- vedu-backend/database/migrations/2024_08_19_151507_create_chats_table.php
+++ vedu-backend/database/migrations/2024_08_19_151507_create_chats_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->text('message');
+            $table->timestamp('timestamp')->useCurrent();
         });
     }
 
