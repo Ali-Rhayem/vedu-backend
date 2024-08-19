139ff946de9e534928ab01359f33e61b223f12c9
Alirh1 <alirhayem.1212@gmail.com>
2024-08-19 18:39:18 +0300

    added fillabe array to submission model

--- vedu-backend/app/Models/Submission.php
+++ vedu-backend/app/Models/Submission.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Submission extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'assignment_id', 'student_id', 'submission_text', 'file_url', 'submitted_at'
+    ];
 }
