e1a4524f5310f880a8e717cf8331bd1a4efb2128
Alirh1 <alirhayem.1212@gmail.com>
2024-08-19 18:05:47 +0300

    added course student

---
+++ vedu-backend/app/Http/Controllers/CourseStudentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CourseStudent;
+use App\Http\Requests\StoreCourseStudentRequest;
+use App\Http\Requests\UpdateCourseStudentRequest;
+
+class CourseStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCourseStudentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CourseStudent $courseStudent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CourseStudent $courseStudent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCourseStudentRequest $request, CourseStudent $courseStudent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CourseStudent $courseStudent)
+    {
+        //
+    }
+}
