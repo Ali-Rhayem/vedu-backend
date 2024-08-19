1f8930e212de2ecd9414ab568b74ef3476dd20a0
Alirh1 <alirhayem.1212@gmail.com>
2024-08-19 18:36:20 +0300

    updated migrations for submissions table

--- vedu-backend/database/migrations/2024_08_19_153439_create_submissions_table.php
+++ vedu-backend/database/migrations/2024_08_19_153439_create_submissions_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('submissions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('assignment_id')->constrained('assignments')->onDelete('cascade');
+            $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
+            $table->text('submission_text')->nullable();
+            $table->string('file_url')->nullable();
+            $table->dateTime('submitted_at')->nullable();
             $table->timestamps();
         });
     }
