da38da5f26f3c816fd35f52725b8ba5f9f5915a4
Alirh1 <alirhayem.1212@gmail.com>
2024-08-19 18:50:22 +0300

    added chrome_extension_summaries migration , model and controller

---
+++ vedu-backend/app/Http/Controllers/ChromeExtensionSummaryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChromeExtensionSummary;
+use App\Http\Requests\StoreChromeExtensionSummaryRequest;
+use App\Http\Requests\UpdateChromeExtensionSummaryRequest;
+
+class ChromeExtensionSummaryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreChromeExtensionSummaryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ChromeExtensionSummary $chromeExtensionSummary)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ChromeExtensionSummary $chromeExtensionSummary)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateChromeExtensionSummaryRequest $request, ChromeExtensionSummary $chromeExtensionSummary)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ChromeExtensionSummary $chromeExtensionSummary)
+    {
+        //
+    }
+}
---
+++ vedu-backend/app/Models/ChromeExtensionSummary.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChromeExtensionSummary extends Model
+{
+    use HasFactory;
+}
