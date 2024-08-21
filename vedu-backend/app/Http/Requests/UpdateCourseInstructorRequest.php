c751cc2b90795284b4fe61899680a94656e9ff29
Alirh1 <alirhayem.1212@gmail.com>
2024-08-21 14:45:22 +0300

    added validation to UpdateCourseInstructorRequest

--- vedu-backend/app/Http/Requests/UpdateCourseInstructorRequest.php
+++ vedu-backend/app/Http/Requests/UpdateCourseInstructorRequest.php
@@ -11,7 +11,7 @@ class UpdateCourseInstructorRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,6 +23,8 @@ class UpdateCourseInstructorRequest extends FormRequest
     {
         return [
             //
+            'course_id' => 'required|exists:courses,id',
+            'instructor_id' => 'required|exists:users,id',
         ];
     }
 }
