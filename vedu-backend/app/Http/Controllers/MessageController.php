1d7e5326105a189250569736e58f5e7cc53d1373
Alirh1 <alirhayem.1212@gmail.com>
2024-08-22 15:29:55 +0300

    removed Create function from MessageController

--- vedu-backend/app/Http/Controllers/MessageController.php
+++ vedu-backend/app/Http/Controllers/MessageController.php
@@ -19,14 +19,6 @@ class MessageController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreMessageRequest $request)
