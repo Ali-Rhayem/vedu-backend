d4955b902c6c5e7a167c1e82f4fbe59b5efe7a41
Alirh1 <alirhayem.1212@gmail.com>
2024-08-24 18:16:24 +0300

    initialized controller and model for SubmissionGrade

---
+++ vedu-backend/app/Http/Controllers/SubmissionGradeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubmissionGrade;
+use App\Http\Requests\StoreSubmissionGradeRequest;
+use App\Http\Requests\UpdateSubmissionGradeRequest;
+
+class SubmissionGradeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSubmissionGradeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SubmissionGrade $submissionGrade)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SubmissionGrade $submissionGrade)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSubmissionGradeRequest $request, SubmissionGrade $submissionGrade)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SubmissionGrade $submissionGrade)
+    {
+        //
+    }
+}
