fb4e6ed5c3a15860dc5e89cfddeba80d1ebbb6d3
Alirh1 <alirhayem.1212@gmail.com>
2024-08-30 14:02:58 +0300

    added topic id to assignments table migration

---
+++ vedu-backend/database/migrations/2024_08_30_105829_add_topic_id_to_assignments_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('assignments', function (Blueprint $table) {
+            $table->foreignId('topic_id')->nullable()->constrained('topics')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('assignments', function (Blueprint $table) {
+            $table->dropForeign(['topic_id']);
+            $table->dropColumn('topic_id');
+        });
+    }
+};
