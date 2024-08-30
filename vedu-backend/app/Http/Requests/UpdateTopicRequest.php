6d903e7f9c5b5338f3de61bc888fb348aba78a1c
Alirh1 <alirhayem.1212@gmail.com>
2024-08-30 14:04:43 +0300

    added update topic request

---
+++ vedu-backend/app/Http/Requests/UpdateTopicRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateTopicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // Allow the request to be authorized
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('topics')->ignore($this->route('topic')),
+            ],
+        ];
+    }
+}
