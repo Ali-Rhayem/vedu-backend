cd41ef98b9b3c03fba2b0586320ea2abf91ee43f
Alirh1 <alirhayem.1212@gmail.com>
2024-09-18 15:04:47 +0300

    updated course_table migration <> added class_code attribute

--- vedu-backend/database/migrations/2024_08_19_144901_create_courses_table.php
+++ vedu-backend/database/migrations/2024_08_19_144901_create_courses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
+            $table->string('class_code')->unique()->nullable();
             $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
