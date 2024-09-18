09a43e2b0656aad736efd30a9f311a024dd1f927
Alirh1 <alirhayem.1212@gmail.com>
2024-09-18 15:45:19 +0300

    updated CourseStudentController <> cleaned the controller from comments

--- vedu-backend/app/Http/Controllers/CourseStudentController.php
+++ vedu-backend/app/Http/Controllers/CourseStudentController.php
@@ -11,30 +11,14 @@ use Illuminate\Http\Request;
 
 class CourseStudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
         $courseStudents = CourseStudent::all();
         return response()->json($courseStudents);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreCourseStudentRequest $request)
     {
-        //
         $existingStudent = CourseStudent::where('course_id', $request->course_id)
             ->where('student_id', $request->student_id)
             ->first();
@@ -59,28 +43,14 @@ class CourseStudentController extends Controller
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(CourseStudent $courseStudent)
     {
         return response()->json($courseStudent);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(CourseStudent $courseStudent)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateCourseStudentRequest $request, CourseStudent $courseStudent)
     {
-        //
         $courseStudent->update($request->validated());
         return response()->json([
             'message' => 'Student updated successfully',
@@ -88,12 +58,8 @@ class CourseStudentController extends Controller
         ], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(CourseStudent $courseStudent)
     {
-        //
         $courseStudent->delete();
         return response()->json(['message' => 'student deleted successfully']);
     }
