a5dfb7e7ba6fc001ec63324954770782e364a04f
Alirh1 <alirhayem.1212@gmail.com>
2024-09-18 15:18:45 +0300

    updated CourseStudentController <> added join class function

--- vedu-backend/app/Http/Controllers/CourseStudentController.php
+++ vedu-backend/app/Http/Controllers/CourseStudentController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\CourseStudent;
 use App\Http\Requests\StoreCourseStudentRequest;
 use App\Http\Requests\UpdateCourseStudentRequest;
+use App\Models\Course;
+use App\Models\CourseInstructor;
+use Illuminate\Http\Request;
 
 class CourseStudentController extends Controller
 {
@@ -36,8 +39,16 @@ class CourseStudentController extends Controller
             ->where('student_id', $request->student_id)
             ->first();
 
+        $existingInstructor = CourseInstructor::where('course_id', $request->course_id)
+            ->where('instructor_id', $request->student_id)
+            ->first();
+
         if ($existingStudent) {
-            return response()->json(['message' => 'Student is already assigned to this course'], 409);
+            return response()->json(['message' => 'Already student in this course'], 409);
+        }
+
+        if($existingInstructor) {
+            return response()->json(['message' => 'Already Instructor in this course'], 409);
         }
 
         $courseStudent = CourseStudent::create($request->validated());
@@ -74,7 +85,7 @@ class CourseStudentController extends Controller
         return response()->json([
             'message' => 'Student updated successfully',
             "course_instructor" => $courseStudent
-        ],200);
+        ], 200);
     }
 
     /**
@@ -87,9 +98,13 @@ class CourseStudentController extends Controller
         return response()->json(['message' => 'student deleted successfully']);
     }
 
+
     public function getCourseStudents($course_id)
     {
-        $courseStudents = CourseStudent::where('course_id', $course_id)->get();
+        $courseStudents = CourseStudent::where('course_id', $course_id)
+            ->with('student')
+            ->get();
+
         return response()->json($courseStudents);
     }
 
@@ -103,4 +118,38 @@ class CourseStudentController extends Controller
         return response()->json($studentCourses);
     }
     
+
+
+    public function joinClass(Request $request)
+    {
+        $validatedData = $request->validate([
+            'class_code' => 'required|string|exists:courses,class_code',
+            'student_id' => 'required|exists:users,id',
+        ]);
+
+        $course = Course::where('class_code', $validatedData['class_code'])->first();
+
+        $existingStudent = CourseStudent::where('course_id', $course->id)
+            ->where('student_id', $validatedData['student_id'])
+            ->first();
+
+        $existingInstructor = CourseInstructor::where('course_id', $course->id)
+            ->where('instructor_id', $validatedData['student_id'])
+            ->first();
+
+        if ($existingStudent) {
+            return response()->json(['message' => 'Already student in this class'], 409);
+        }
+
+        if ($existingInstructor) {
+            return response()->json(['message' => 'Already instructor in this class'], 409);
+        }
+
+        $courseStudent = CourseStudent::create([
+            'course_id' => $course->id,
+            'student_id' => $validatedData['student_id'],
+        ]);
+
+        return response()->json(['message' => 'Successfully joined the class', 'course' => $courseStudent], 201);
+    }
 }
