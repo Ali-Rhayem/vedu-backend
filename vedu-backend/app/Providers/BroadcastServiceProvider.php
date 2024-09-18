58f2dcb9badc698ecc77009ab321b710fde3e52f
Alirh1 <alirhayem.1212@gmail.com>
2024-09-18 15:15:01 +0300

    updated BroadcastServiceProvider <> added middleware auth:api to routes

--- vedu-backend/app/Providers/BroadcastServiceProvider.php
+++ vedu-backend/app/Providers/BroadcastServiceProvider.php
@@ -12,8 +12,9 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Broadcast::routes();
+        Broadcast::routes(['middleware' => ['auth:api']]);
 
         require base_path('routes/channels.php');
     }
 }
+
