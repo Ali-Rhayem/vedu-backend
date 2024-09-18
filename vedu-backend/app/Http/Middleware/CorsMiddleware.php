63bd6f5e74f0c4a6366eae6f7fb0f8c452acb628
Alirh1 <alirhayem.1212@gmail.com>
2024-09-18 15:17:18 +0300

    initialized cors middelare <> created CorsMiddleware

---
+++ vedu-backend/app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->getMethod() === "OPTIONS") {
+            return response('', 204)
+                ->header('Access-Control-Allow-Origin', 'http://localhost:3000') 
+                ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin');
+        }
+    
+        $response = $next($request);
+    
+        $response->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+                 ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+                 ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin');
+    
+        return $response;
+    }
+    
+    
+}
