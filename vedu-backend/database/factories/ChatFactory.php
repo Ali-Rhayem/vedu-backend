056c12120995444a82cca67c24b42933ff009aeb
Alirh1 <alirhayem.1212@gmail.com>
2024-09-20 23:56:54 +0300

    updated chat factory <> added chat attributes to the definition function

--- vedu-backend/database/factories/ChatFactory.php
+++ vedu-backend/database/factories/ChatFactory.php
@@ -2,22 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\Chat;
+use App\Models\User;
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Chat>
- */
 class ChatFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Chat::class;
+
+    public function definition()
     {
         return [
-            //
+            'course_id' => Course::factory(),
+            'sender_id' => User::factory(),
+            'receiver_id' => User::factory(),
         ];
     }
 }
