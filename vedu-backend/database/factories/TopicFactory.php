b6c0d1087840a731be64984bdbe19ca91d54ee5a
Alirh1 <alirhayem.1212@gmail.com>
2024-09-20 23:57:57 +0300

    updated topic factory <> added topic attributes to the definition function

--- vedu-backend/database/factories/TopicFactory.php
+++ vedu-backend/database/factories/TopicFactory.php
@@ -2,22 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Topic>
- */
 class TopicFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = \App\Models\Topic::class;
+
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word, 
+            'course_id' => Course::factory(),
         ];
     }
 }
